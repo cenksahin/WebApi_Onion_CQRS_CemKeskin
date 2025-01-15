d7a1bab2f389bfbf90d11cee683276ddbf7a590d
Cenk Şahin <cenk.sahin@hotmail.com.tr>
2025-01-15 21:46:27 +0300

    entityler oluşturuldu

---
+++ Core/YoutubeApi.Domain/Common/EntityBase.cs
@@ -0,0 +1,9 @@
+﻿namespace YoutubeApi.Domain.Common
+{
+    public class EntityBase : IEntityBase
+    {
+        public int Id { get; set; }
+        public DateTime CreateDate { get; set; } = DateTime.Now;
+        public bool IsDeleted { get; set; } = false;
+    }
+}
