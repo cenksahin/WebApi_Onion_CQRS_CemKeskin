208b739299cbf602997de0ae398f46cbfac16e02
Cenk Şahin <cenk.sahin@hotmail.com.tr>
2025-01-16 20:39:37 +0300

    write repositry yapıdı

--- Infrastructure/YoutubeApi.Persistence/Repositories/WriteRepository.cs
+++ Infrastructure/YoutubeApi.Persistence/Repositories/WriteRepository.cs
@@ -1,12 +1,44 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.EntityFrameworkCore;
+using YoutubeApi.Application.Interfaces.Repositories;
+using YoutubeApi.Domain.Common;
+using YoutubeApi.Persistence.Context;
+
 
 namespace YoutubeApi.Persistence.Repositories
 {
-    public class WriteRepository
+    public class WriteRepository<T> : IWriteRepository<T> where T : class, IEntityBase, new()
     {
+        private readonly AppDbContext _appDbContext;
+        public WriteRepository(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+
+        private DbSet<T> Table { get => _appDbContext.Set<T>(); }
+
+
+        public async Task AddAsync(T entity)
+        {
+            await Table.AddAsync(entity);
+        }
+
+        public async Task AddRangeAsync(IList<T> entities)
+        {
+            await Table.AddRangeAsync(entities);
+        }
+
+        public async Task HardDeleteAsync(T entity)
+        {
+            //remove'nin asyc'nu olmadığı için biz yazdık.
+            await Task.Run(() => Table.Remove(entity));
+        }
+
+        public async Task<T> UpdateAsync(T entity)
+        {
+            //update'nin asyc'nu olmadığı için biz yazdık.
+            await Task.Run(() => Table.Update(entity));
+
+            return entity;
+        }
     }
 }
